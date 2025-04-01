adbbb4b24313a49e7ab65dda73d4ff0460e479f1
Alolong <ddten123@gmail.com>
2025-04-01 10:16:14 +0900

    test

---
+++ test/test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int Number1 = 1;
+	int Number2 = 2;
+
+	cout << (Number1 + Number2) <<  endl;
+	cout << (Number1 - Number2) <<  endl;
+	cout << (Number1 * Number2) <<  endl;
+	cout << (Number1 / Number2) <<  endl;
+
+
+	int Number3 = 1;
+	int Number4 = 2;
+
+	cout << (Number3 + Number4) << endl;
+	cout << (Number3 - Number4) << endl;
+	cout << (Number3 * Number4) << endl;
+	cout << (Number3 / Number4) << endl;
+
+	return 0;
+}
